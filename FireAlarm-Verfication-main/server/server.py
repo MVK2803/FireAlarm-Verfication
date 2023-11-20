99491078a00ad2a0fe30725a138e2201f13abb80
Dave Paul Joseph <davepauljoseph@gmail.com>
2023-11-20 22:01:27 +0530

    testCommit

---
+++ FireAlarm-Verfication-main/server/server.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import fire_fun
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/', methods=['POST'])
+def home():
+    print("REC")
+    try:
+        # Extract data from the request
+        input_data = request.json['input_data']
+        testInput=[]
+        trigger_alarm = request.json['trigger_alarm']
+        for i in input_data:
+            testInput.append(float(input_data[i]))
+        print(testInput)
+        # Perform prediction
+        result = fire_fun.predictFalseAlarm(*testInput)
+        print(result)
+        return jsonify(result)
+    except Exception as e:
+        print(f"Error: {str(e)}")
+        return jsonify({"error": "An error occurred"}), 500
+
+@app.route('/hello')
+def hello_world():
+    return 'Hello, World!'
+
+
